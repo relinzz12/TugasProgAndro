802cab3381defbfc480c3169a17f8b6c3164af86
tes <albie.ferdinata@ti.ukdw.ac.id>
2020-03-06 16:24:53 +0700

    ProgAndro

--- app/src/main/java/com/example/projectandroid/SecondActivity.java
+++ app/src/main/java/com/example/projectandroid/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.example.projectandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
@@ -8,7 +9,11 @@ public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_second);
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+
     }
 }
